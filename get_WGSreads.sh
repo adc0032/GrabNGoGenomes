d4bdacc73c1ca4f846faf57a09779e10a28e0806
Amanda D. Clark <adc0032@auburn.edu>
2019-04-15 00:07:05 -0500

    adding initial script for user input of organism name and searching for sequencing run. Output example from the search term "dog"

---
+++ get_WGSreads.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $1 == "-h" ]]; then
+
+    echo "Enter search query organism for lists of sequence run accession information."
+    echo "Script file with a tab delimited table including SRA, /# of bases, Library Type, Sample"
+
+
+elif [[ $# -lt 1 ]]; then
+
+        echo "Please provide Organism for a table of NCBI's sequence information. Try -h for help"
+        exit 0
+else
+    touch $1.SRA_info.txt
+    esearch -db sra -query "$1 [ORGN]"|
+    efetch -format runinfo -mode xml|
+    xtract -pattern Row -tab "\t" -sep "," -def "BLANK" -element Run bases LibraryStrategy Sample|
+    awk -F "\t" '$3=="WGS"{print $0}' > $1.SRA_info.txt
+fi
+
