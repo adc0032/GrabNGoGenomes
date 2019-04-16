4142224c4d39b18dbc88a1c092126610108a300e
Amanda D. Clark <adc0032@auburn.edu>
2019-04-15 23:15:26 -0500

    adding argument test for more than one positional argument and help messages for script usage. Need to add test that $2 is a file.

--- seq_pull.sh
+++ seq_pull.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 
-##Modules and Variables
+if [[ $1 == "-h" ]]; then
 
-module load sratooklkit.2.9.6-centos_linux64
-#for redundant runs making variable for date to include on directory for sequence output
-cdate= $(date | awk'{OFS="_"}{print $2,$3}')
+    echo "Usage: seq_pull.sh [ORGANISM] [ORGANISM.run_accession.DATE.txt]"
+    echo "Include organism name and file with sequence run accessions for downloading associated sequences to a directory"
+    echo "Run get_WGSreads.sh USER_ORGANISM to get SRR list to input into seq_pull.sh"
 
-mkdir seq_files_$cdate
+elif [[ $# -lt 2 ]]; then
 
-cd seq_files_$cdate
+    echo "Please provide an organism and the associated run accession list file to download sequences. Try -h for help"
+    exit 0
 
-for run in $(cat run_list.txt)
-do
+else
+    ##Modules and Variables
+    module load sratooklkit.2.9.6-centos_linux64
+    #for redundant runs making variable for date to include on directory for sequence output
+    cdate= $(date | awk'{OFS="_"}{print $2,$3}')
+
+    mkdir seq_files_$cdate
+
+    cd seq_files_$cdate
+
+    for run in $(cat run_list.txt)
+    do
     fastq-dump -I $run
-done
+    done
 
