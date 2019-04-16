fc20a1f58cde69e11ae63ab02bf281b91c9a712d
Amanda D. Clark <adc0032@auburn.edu>
2019-04-16 14:00:08 -0500

    Adding test file that includes edits to accept scientific name at commandline with script, updated help messsage to include usage

---
+++ get_WGSreads_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $1 == "-h" ]]; then
+####Telling user that if using scientific name, USER_ORGANISM must be in quotes
+    echo "Usage: get_WGSread.sh USER_ORGANISM or \"USR_Genus USR_species\""
+    echo "Enter search query organism for lists of sequence run accession information; If entering name with more than one word, use quotes"
+    echo "Example: get_WGSread.sh dog | get_WGSread.sh \"Canis lupis familaris\""
+    echo "Script will provide a tab delimited table including SRR & SRS, library information, Scientific Name, sequencer informoation, and consent information for the organism"
+    echo "Script will also create a file with a list of run IDs for WGS data from queried organism; use awk/grep on provided SRA_info file to create filtered lists to provide to sequence downloader, seq_pull.sh "
+
+
+
+elif [[ $# -lt 1 ]]; then
+
+        echo "Please provide organism for a table of NCBI's sequence information. Try -h for help"
+        exit 0
+else
+###assigning positional argument to variable "org" that can be used for further manipulation 
+    org=$1
+    label=$(echo $org|awk '{print $1}')
+    cdate=$(date|awk '{OFS="_"}{print $2,$3}')
+    echo "Run_ID    Lib_Size(MB)    Lib_Type    Sample_ID    Scientific_Name    Sequencing_Platform    Model    Consent        $cdate" > $label.SRA_info_$cdate.txt
+    esearch -db sra -query "$org [ORGN]"|
+    efetch -format runinfo -mode xml |
+    xtract -pattern Row -tab "\t" -sep "," -def "BLANK" -element Run size_MB LibraryStrategy Sample ScientificName Platform Model Consent |
+    awk -F "\t" '$3=="WGS"{print $0}' >> $label.SRA_info_$cdate.txt
+fi
+
+
+awk '{print $org}' $label.SRA_info_$cdate.txt| tail -n +2 > $label.run_accession.$cdate.txt
+
+Entries=$(tail -n +2 $label.SRA_info_$cdate.txt | wc -l)
+echo "$Entries entries found. Run IDs for sequence download is availabe as $label.run_accession.$cdate.txt. See $label.SRA_info_$cdate.txt for more information"
