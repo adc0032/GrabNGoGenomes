52e00615949d367234c1680b711351753132213c
Amanda D. Clark <adc0032@auburn.edu>
2019-04-17 23:52:10 -0500

    updated help message to match complexity of script. adding exit message to decrease stdout messages that look erroneous

--- get_WGSreads.sh
+++ get_WGSreads.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [[ $1 == "-h" ]]; then
-
-    echo "Enter search query organism for lists of sequence run accession information."
-    echo "Script will provide a tab delimited table including SRR & SRS, library information, Scientific Name, sequencer informoation, and consent information for the organism"
-    echo "Script will also create a file with a list of run IDs for WGS data from queried organism; use awk/grep on provided SRA_info file to create filtered lists to provide to sequence downloader, seq_pull.sh "
-
+if [[ $1 == "-h" || $1 == "" ]]; then
+    echo "Usage: get_SeqReads [-F|-P] [USER_ORGANISM | \"USR_Genus USR_species\] [WGS|WXS|AMPL$
+    echo "Enter search query organism for lists of sequence run archive information; If enteri$
+    echo "Examples: get_SeqReads -P dog | get_SeqReads -F \"Canis lupis familaris\" RAD-Seq"
+    echo "In partial mode (-P), script will provide a tab delimited table including SRR & SRS,$
+    echo "use awk/grep on provided SRA_info file to create filtered lists to provide to sequen$
+    echo "When running get_SeqReads in full mode (-F) library strategy must be included after $
+    echo "Full mode creates a file with a list of run IDs from queried organism and downloads $
+    exit 0
 
 
 elif [[ $# -lt 1 ]]; then
