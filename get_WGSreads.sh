dd8ea321e7e6da5ea8c1118ced9d19e05b99e31d
Amanda D. Clark <adc0032@auburn.edu>
2019-04-18 00:22:54 -0500

    Adding usage of partial run of script. produces folder with full list of search results and list filtered by indicated sequencing choice taken as a third argument to the script fixed line truncation from copy

--- get_WGSreads.sh
+++ get_WGSreads.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
 
 if [[ $1 == "-h" || $1 == "" ]]; then
-    echo "Usage: get_SeqReads [-F|-P] [USER_ORGANISM | \"USR_Genus USR_species\] [WGS|WXS|AMPL$
-    echo "Enter search query organism for lists of sequence run archive information; If enteri$
+    echo "Usage: get_SeqReads [-F|-P] [USER_ORGANISM | \"USR_Genus USR_species\] [WGS|WXS|AMPLICON|RNA-Seq|RAD-Seq|ChIP-Seq|Hi-C]"
+    echo "Enter search query organism for lists of sequence run archive information; If entering name with more than one word, use quotes"
     echo "Examples: get_SeqReads -P dog | get_SeqReads -F \"Canis lupis familaris\" RAD-Seq"
-    echo "In partial mode (-P), script will provide a tab delimited table including SRR & SRS,$
-    echo "use awk/grep on provided SRA_info file to create filtered lists to provide to sequen$
-    echo "When running get_SeqReads in full mode (-F) library strategy must be included after $
-    echo "Full mode creates a file with a list of run IDs from queried organism and downloads $
+    echo "In partial mode (-P), script will provide a tab delimited table including SRR & SRS, library information, Scientific Name, sequencer informoation, and consent information for the organism;"
+    echo "use awk/grep on provided SRA_info file to create filtered lists to provide to sequence downloader, seq_pull.sh"
+    echo "When running get_SeqReads in full mode (-F) library strategy must be included after search organism.
+    echo "Full mode creates a file with a list of run IDs from queried organism and downloads specified sequence reads; This script requires SRA Toolkit, please load local/cluster module."
     exit 0
 
 
 elif [[ $# -lt 3 ]]; then
 
-    echo "Please indicate -F or -P for full or partial run, followed by a search organism,$
-    exit 0
+        echo "Please indicate -F or -P for full or partial run, followed by a search organism, followed by a library strategy for a table of NCBI's sequence information. Try -h for help"
+        exit 0
 
-else
-    cdate=$(date|awk '{OFS="_"}{print $2,$3}')
-    echo "Run_ID    Lib_Size(MB)    Lib_Type    Sample_ID    Scientific_Name    Sequencing_Platform    Model    Consent        $cdate" > $1.SRA_info_$cdate.txt
-    esearch -db sra -query "$1 [ORGN]"|
-    efetch -format runinfo -mode xml |
-    xtract -pattern Row -tab "\t" -sep "," -def "BLANK" -element Run size_MB LibraryStrategy Sample ScientificName Platform Model Consent |
-    awk -F "\t" '$3=="WGS"{print $0}' >> $1.SRA_info_$cdate.txt
+elif [[ $1 == "-P" ]]; then
+        org=$2
+        genus=$(echo $org|awk '{print $1}')
+        species=$(echo $org|awk '{print $2}')
+        cdate=$(date|awk '{OFS="_"}{print $2,$3}')
+        mkdir $genus$species~$cdate
+        cd $genus$species~$cdate
+        echo "Moving into $genus$species~$cdate/ ..."
+        echo ""
+        echo ""
+        echo "========================================================================================================================="
+        echo "Running SomeCreativelyNamedProgram in partial mode. All SRA search results for $org with public consent and SRR prefix will output to $genus$species~full_SRA_info_$cdate.txt"
+        echo "Use awk/grep to filter output and run seq_pull.sh USR_ORGN. Prompt will ask for SRA_info file to pull SRR IDs, default is last created SRA_info file for indicated USR_ORGN."
+        echo ""
+        echo ""
+        echo "Run_ID    Lib_Size(MB)    Lib_Type    Sample_ID    Scientific_Name    Sequencing_Platform    Model    Consent        $cdate" > $genus$species~full_SRA_info_$cdate.txt
+        esearch -db sra -query "$org [ORGN]"|
+        efetch -format runinfo -mode xml |
+        xtract -pattern Row -tab "\t" -sep "," -def "BLANK" -element Run size_MB LibraryStrategy Sample ScientificName Platform Model Consent|
+        awk -F "\t" '$8 == "public" {print $0}' | awk -F "\t" '/^SRR/ {print $0}'>> $genus$species~full_SRA_info_$cdate.txt
+        Entries=$(tail -n +2 $genus$species~full_SRA_info_$cdate.txt | wc -l)
+        echo "$Entries entries found. See $genus$species~full_SRA_info_$cdate.txt for more information."
+        echo ""
+        echo "To decrease downloading time, filtered output file for sequencing strategy indicated will be written to $genus$species~filtered_SRA_info_$cdate.txt. Use this file to create a list of SRR IDs as input for seq_pull.sh"
+        echo "See line 87 of this script for command structure to produce input list for seq_pull.sh"
+        strat=$3
+        echo ""
+        echo "Strategy used: $strat"
+        awk '$3 == "'$strat'" {print $0}' $genus$species~full_SRA_info_$cdate.txt >> $genus$species~filtered_SRA_info_$cdate.txt
+        Entries=$(tail -n +2 $genus$species~filtered_SRA_info_$cdate.txt | wc -l)
+        echo "$Entries entries retain after filtering for $strat reads. See $genus$species~filtered_SRA_info_$cdate.txt for more information."
+        echo ""
+        exit 0
+
 fi
 
 
